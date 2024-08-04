f9e2c372c1cddf6e30fc01d624d1100fb2be2d10
Akhil Narayanan <akhil.narayanan@se.com>
2024-08-04 15:54:54 +0530

    🔥 removed web-nuxt ui

--- web-nuxt/components/CircleLoad.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="flex items-center justify-center h-screen">
-        <span class="text-primary loading loading-dots loading-lg"></span>
-    </div>
-</template>
-<script setup lang="ts">
-</script>
--- web-nuxt/pages/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <NuxtLink to="/login">Login</NuxtLink>
-    <NuxtLink to="/signup">Signup</NuxtLink>
-    <NuxtLink to="/dashboard">Dashboard</NuxtLink>
-</template>
-
-<script setup lang="ts">
-</script>
